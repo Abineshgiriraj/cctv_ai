ba01242380fa77e43f106c3ff32ab9b917ef4fa7
Abinesh G <05abinesh06@gmail.com>
2026-09-18 11:21:15 +0530

    Bust UI cache for pagination fixes

--- cctv_Ai/ui_common.php
+++ cctv_Ai/ui_common.php
@@ -145,7 +145,7 @@ window.CCTV_UI_CONFIG = {
     cameras: <?= json_encode($cameras, JSON_UNESCAPED_SLASHES) ?>
 };
 </script>
-<script src="assets/app_fixed.js?v=2"></script>
+<script src="assets/app_fixed.js?v=3"></script>
 <?php foreach ($extraScripts as $src): ?>
 <script src="<?= e($src) ?>"></script>
 <?php endforeach; ?>
